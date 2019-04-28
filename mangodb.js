d3ebabc55294ce544c92daea8a4f378935979250
Ed <Zempel>
2019-04-27 21:50:26 -0500

    add many to tasks

---
+++ mangodb.js
@@ -0,0 +1,65 @@
+// alt+shift+f formats code
+console.log('How is it going?')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const dataBaseName = 'firstDatabase'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        console.log('Unable to connect to the database.')
+    }
+    const db = client.db(dataBaseName)
+
+    // db.collection('users').insertOne({
+    //     name: 'Steve',
+    //     address: '1204 Western Ave',
+    //     city: 'Xandadu',
+    //     state: 'Kentucky',
+    //     apples: 14
+    //     })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Sally',
+    //         address: '123 University Ave'
+    //     },
+    //     {
+    //         name: 'snoopy',
+    //         address: '999 Stilton ST'
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         console.log('Unable to save to the database.')
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    //insert 3 tasks into a new tasks collection
+    // insert 3 documents
+    db.collection('tasks').insertMany([
+        {
+            task: 'eat vegeatables',
+            dueDate: 'Monday',
+            completed: false
+        },
+        {
+            task: 'buy a puppy',
+            dueDate: 'Wednesday',
+            completed: false
+        },
+        {
+            task: 'go to the moon',
+            dueDate: 'Friday',
+            completed: true
+        }
+    ],(error,result) =>{
+        if(error){
+            console.log('Unable to save to the database.')
+        }
+        console.log(result.ops)
+    })
+
+
+})
